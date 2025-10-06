ff3eb7a9bf22630f2b0ccbe6d204b07651995021
Daavidmp <cod.davidmp@gmail.com>
2025-10-06 18:01:28 +0200

    Delete Proyecto-intermodular-develop directory

--- Proyecto-intermodular-develop/Java-Postgres-CRUD-main/src/conection/test.java
+++
@@ -1,6 +0,0 @@
-package conection;
-public class test {
-    public static void main(String[] args) {
-        System.out.println("funciona automaticamente :)");
-    }
-}
--- Proyecto-intermodular-develop/README.md
+++
@@ -1,6 +0,0 @@
-# Proyecto-intermodular
-grupo de Diego de proyecto
-
-
-Proyecto que tocaremos:
-https://github.com/Dennis290699/Java-Postgres-CRUD/tree/main
